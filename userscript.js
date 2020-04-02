224846ec471b68b9814176fa782d24dffd4e5892
Karl Beecken <karl.beecken@icloud.com>
2020-04-02 10:03:08 +0200

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name 💩-Cloud
+// @version 1.0
+// @description made with sh*t in potsdam
+// @include https://schul-cloud.org/*
+// @downloadURL
+// ==/UserScript==
+
+// modify footer to correct the given false statements, there is no ❤️ anymore!
+
+document.getElementsByClassName('made-with-love')[0].innerHTML = "Made with <span class=\"heart\">💩</span> in Potsdam"
+
+document.getElementsByClassName("imprint")[0].children.item(0).children.item(1).innerHTML = "&copy; 2020 💩-Cloud"
